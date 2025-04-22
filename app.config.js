6582f248ce37d30b6cc90de5f07fbb38e4944f1b
Eduardo Prestes <eduardo.prestes@facens.br>
2025-04-22 10:36:40 -0300

    feat(env): adiciona suporte a IP dinâmico via dotenv para integração com backend local

--- app.config.js
+++ app.config.js
@@ -1,9 +1,10 @@
+import 'dotenv/config';
 export default () => ({
     expo: {
       name: 'black_box',
       slug: 'black_box',
       extra: {
-        API_URL: process.env.API_URL || 'http://localhost:3000/api',
+        API_URL: process.env.API_URL || 'http://172.16.52.84:3000/api',
       },
     },
   });
